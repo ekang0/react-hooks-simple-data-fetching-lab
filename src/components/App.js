924da57cf8402495ab569cd394ccbc1cd7209c0d
Esther Kang <eskang87@gmail.com>
2022-03-24 09:48:04 -0700

    commit tree c1f6d2976fba873b6cfe341dc7981df2153f1adc

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App(){
+  const [img, setImg] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(data => {
+      setImg(data.message);
+    });
+  }, []);
+
+  if (!img) {
+    return <p>Loading...</p>
+  }; 
+
+  return (
+      <img src={img} alt="A Random Dog" />
+  );
+};
+
+export default App;
